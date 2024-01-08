61508714a950bad3ae29a41303cd0c4a899a2848
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-08 12:39:08 -0300

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Christian Manzaraz on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
